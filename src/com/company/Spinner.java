1ea951beec163136db4a8cd819c3fafc4e18a54c
Emenu Dobamo <birhanu.emenu@gmail.com>
2018-09-25 14:46:00 -0600

    added a spin method

--- src/com/company/Spinner.java
+++ src/com/company/Spinner.java
@@ -1,4 +1,29 @@
+/*Copyright 2018 Principal Software Engineers
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+        and associated documentation files (the "Software"), to deal in the Software without restriction,
+        including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+         and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+         subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all copies or substantial
+         portions of the Software.
+
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+        LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+        WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+         OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+
+
+
 package com.company;
+
+import java.util.Collections;
+import java.util.Random;
+
 /** First version of a game. Game accepts user input for number of coins,
  *  number of spins, and number of coins to be revealed. Also this version
  *  of the game checks for invalid inputs, and initializes so the game is
@@ -10,7 +35,7 @@ package com.company;
  */
 public class Spinner {
 
-    CircularList<Coin> coins;
+    public CircularList<Coin> coins;
     /**
       * Constructs a spinner object which initializes CircularList with the
       * number of coins provided by the user input
@@ -30,13 +55,6 @@ public class Spinner {
     }
 
     public void spin(){
-        Coin temp;
-        int i;
-        temp = coins.get(0);
-        for (i = 0; i < coins.size() - 1; i++)
-            coins.set(i,coins.get(i+1));
-        coins.set(i,temp);
-
-
+        Collections.rotate(coins,new Random().nextInt(coins.size()));
     }
 }
