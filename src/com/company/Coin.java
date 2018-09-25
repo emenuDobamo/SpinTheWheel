eb6dadf7f04e8167f27917106c46b44c716f5990
Emenu Dobamo <birhanu.emenu@gmail.com>
2018-09-25 14:59:30 -0600

    Adds license statement

--- src/com/company/Coin.java
+++ src/com/company/Coin.java
@@ -1,3 +1,24 @@
+/*Copyright 2018 Principal Software Engineers
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+        and associated documentation files (the "Software"), to deal in the Software without restriction,
+        including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+         and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+         subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all copies or substantial
+         portions of the Software.
+
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+        LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+        WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+         OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+
+
+
 package com.company;
 import java.util.Random;
 /** First version of a game. Game accepts user input for number of coins,
@@ -15,9 +36,7 @@ public class Coin {
     private TypeOfSide side;
 
     /**
-     * Constructs a Coin object using random number between 0.0 and 0.9
-     * where 0.0-0.4 assigns TAIL, and if the random value is from 0.5-0.9
-     * then it assigns HEAD.
+     * Constructs a Coin object with the value of either HEAD or TAIL.
      */
     public Coin (){
       Random  rnd = new Random();
